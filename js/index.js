b13cb05d4ea317e0987d869083f56ec207d77446
José Luis Valdiviezo Peña <luisj.valdiviezo20@gmail.com>
2020-05-18 09:11:44 -0500

    Se termino el proyecto, ha excepcion del modo oscuro que tenemos problemas con unos botones.

--- js/index.js
+++ js/index.js
@@ -8,7 +8,6 @@ const moreInfo = document.getElementById('moreInfo');
 const country = document.querySelectorAll('#country');
 
 
-
 darkModeButton.addEventListener('click',()=>{
   header.classList.toggle('darkMode');
   darkModeButton.classList.toggle('darkMode')
@@ -18,14 +17,19 @@ darkModeButton.addEventListener('click',()=>{
   search.classList.toggle('searchDark')
   countrySearch.classList.toggle('searchDark')
   let card = document.querySelectorAll('#card');
+  back.classList.toggle('searchDark')
   card.forEach((c)=>{
     c.classList.toggle('cardDark')
   })
   country.forEach((c)=>{
     c.classList.toggle('searchDark')
   })
-  back.classList.toggle('searchDark')
+  let borbutton = document.querySelectorAll('.bor')
+  borbutton.forEach(b => {
+    b.classList.toggle('searchDark')
+  })
 })
+
 function innerCard(city) {
   main.innerHTML =""
   for (let c of city) {
@@ -94,6 +98,22 @@ async function consultation_info(name) {
     console.log(err)
     })
 }
+async function consultation_alpha(alpha) {
+  console.dir(alpha.innerHTML)
+     await fetch(`https://restcountries.eu/rest/v2/alpha/${alpha.innerHTML}`)
+    .then(res=>res.json())
+    .then(res=>{
+     innerborder(res)
+     showMoreInfoBorder()
+     const back = document.getElementById('back');
+     back.addEventListener('click',()=>{
+      showMoreInfo()
+})
+    })
+   .catch((err)=>{
+    console.log(err)
+    })
+}
 async function consultation_region(region) {
      await fetch(`https://restcountries.eu/rest/v2/region/${region}`)
     .then(res=>res.json())
@@ -102,7 +122,6 @@ async function consultation_region(region) {
       let card = document.querySelectorAll('#card');
       card.forEach(c => {
         c.addEventListener('click', function(){
-          console.dir(this)
           let name = this.children[1].children[0].innerText;
           consulta_info(name)
         })
@@ -115,7 +134,9 @@ async function consultation_region(region) {
 function innerInfo(c) {
   c = c[0]
   let len = ""
-  c.languages.forEach(l => len += l.name + " ")
+  for (const l of c['languages']) {
+    len += " " + l.name
+  }
   moreInfo.innerHTML = `
     <div class="container"><button class="btn mt-3" id="back">Back</button></div>
      <div class="d-flex container justify-content-center align-items-center" id="subcontainer">
@@ -134,17 +155,71 @@ function innerInfo(c) {
           <div id="item2">
            <p class="card-text"><strong>Top Level Domain:</strong>${c.topLevelDomain}</p>
            <p class="card-text"><strong>Currencies:</strong> ${c.currencies[0]['name']}</p>
-           <p class="card-text id="len"></p>
+           <p class="card-text id="len"><strong>Languages:</strong> ${len}</p>
          </div>
          <footer class="d-flex justify-content-center align-items-center mt-5">
            <p class="align-content-center mb-0 mr-2">Border Countries:</p>
-           <div>
-           ${c.borders}
+           <div id="borders">
+           
            </div>
          </footer>
        </div>
      </div>
   `
+  let bor = document.getElementById('borders')
+  for (const b of c['borders']) {
+      bor.innerHTML += `
+      <button class="bor btn">${b}</button>
+      `
+  }
+  let borbutton = document.querySelectorAll('.bor')
+  borbutton.forEach(b => {
+    b.addEventListener('click', function(){consultation_alpha(this)})
+  })
+}
+function innerborder(c) {
+  let len = ""
+  for (const l of c['languages']) {
+    len += " " + l.name
+  }
+  moreInfo.innerHTML = `
+    <div class="container"><button class="btn mt-3" id="back">Back</button></div>
+       <figure class="" id="wrapperImg">
+         <img src="${c.flag}" alt="">
+       </figure>
+       <div class="" id="wrapperInfo">
+            <h4 class="mb-4">${c.name}</h4>
+         <div id="item1">
+          <p class="card-text"><strong>Native Name:</strong> ${c.nativeName}</p>
+          <p class="card-text"><strong>Population:</strong> ${c.population}</p>
+          <p class="card-text"><strong>Region:</strong> ${c.region}</p>
+          <p class="card-text"><strong>Sub Region:</strong> ${c.subregion}</p>
+          <p class="card-text"><strong>Capital:</strong> ${c.capital}</p>
+         </div>
+          <div id="item2">
+           <p class="card-text"><strong>Top Level Domain:</strong>${c.topLevelDomain}</p>
+           <p class="card-text"><strong>Currencies:</strong> ${c.currencies[0]['name']}</p>
+           <p class="card-text id="len"><strong>Languages:</strong> ${len}</p>
+         </div>
+         <footer class="d-flex justify-content-center align-items-center mt-5">
+           <p class="align-content-center mb-0 mr-2">Border Countries:</p>
+           <div id="borders">
+           
+           </div>
+         </footer>
+       </div>
+     </div>
+  `
+  let bor = document.getElementById('borders')
+  for (const b of c['borders']) {
+      bor.innerHTML += `
+      <button class="bor btn">${b}</button>
+      `
+  }
+  let borbutton = document.querySelectorAll('.bor')
+  borbutton.forEach(b => {
+    b.addEventListener('click', function(){consultation_alpha(this)})
+  })
 }
 
 function showMoreInfo() {
@@ -152,6 +227,12 @@ function showMoreInfo() {
     info.classList.toggle('show')
     main.classList.toggle('hidden')
     filter.classList.toggle('hidden')
+}
+function showMoreInfoBorder(){
+  let info = document.getElementById('moreInfo');
+    info.classList.add('show')
+    main.classList.add('hidden')
+    filter.classList.add('hidden')
 }
 
 countrySearch.addEventListener('change',function(){
